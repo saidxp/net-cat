d65eb73b0f994367e0b9441fa3c8404a312a5893
saidxp <aissoulhcen@gmail.com>
2025-02-19 11:59:57 +0100

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module mod
+module test
 
 go 1.23.2
--- main.go
+++ main.go
@@ -7,13 +7,11 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"time"
 )
-type yew struct {
-	str string
-}
+
 type Authentication struct {
 	con map[string]*Link
-
 	mu  sync.RWMutex
 	// chanel to passe the data trought multi processe of data in concurency of go routine !!
 	msg chan Message
@@ -23,12 +21,14 @@ type Link struct {
 	conn net.Conn
 }
 type Message struct {
-	Login string
+	Login   string
 	content string
 }
 
 func main() {
 	port := os.Args[1]
+	// her i reseverd 10 struct inside struct of chan *bufer for stock and limit the number of client who it's connected at the same time !!
+	limiteure := make(chan struct{}, 10)
 	socket, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		fmt.Println("error to connect")
@@ -36,80 +36,102 @@ func main() {
 	}
 	// her i face the problem of Initializasion
 	auth := &Authentication{
-		con: make(map[string]*Link) ,
+		con: make(map[string]*Link),
 		msg: make(chan Message),
 	}
 	go auth.serveclient()
 	for {
 		accept, err := socket.Accept()
+		// i add this to *buffer inside struct chan to reserve place "empty struct"
+		limiteure <- struct{}{}
 		if err != nil {
-			fmt.Println("ello fuck")
+			fmt.Println("Error to connect with client")
 		}
-		go handleconection(accept, auth)
+		go handleconection(accept, auth, limiteure)
 	}
 }
 
 func (ser *Authentication) serveclient() {
-		 
-		for m := range ser.msg {
-		ser.mu.RLock()
-		// her i Merge the name of client and her message !!
-		result := m.Login + m.content
-		for _, link := range ser.con {
-			// her i was face the problem of not send to all client !!
-			_, err := link.conn.Write([]byte(result + "\n"))
-			if err != nil {
-				fmt.Println("Error writing to ",m.Login)
+	ser.mu.Lock()
+	// idont know why this it's work just with loop while he is just one message who is pass in channel!
+	// m := <-ser.message
+	for m := range ser.msg {
+		timestamp := time.Now().Format("[2006-01-02 15:04:05]") //[YYYY-MM-DD HH:MM:SS]
+		// Format message !!
+		result := fmt.Sprintf("%s [%s]: %s", timestamp, m.Login, m.content)
+		for name, link := range ser.con {
+			// her i skiip the user who is send the message !!
+			if name == m.Login {
+				continue
 			}
+			// her i was face the problem of not send to all client !!
+			// the problem of new line ~ !!!
+			_, err := link.conn.Write([]byte("\n" + result + "\n"))
+			if err != nil {
+				fmt.Println("Error writing to ", m.Login)
 			}
 		}
-		ser.mu.RUnlock()
+		for name, link := range ser.con {
+			fmt.Fprintf(link.conn, "%s [%s]: ", timestamp, name)
+		}
+	}
+	ser.mu.Unlock()
 }
 
-func handleconection(client net.Conn, auth *Authentication) {
-	w := yew{
-		str: 'Welcome to TCP-Chat!
-		_nnnn_
-	   dGGGGMMb
-	  @p~qp~~qMb
-	  M|@||@) M|
-	  @,----.JM|
-	 JS^\\__/  qKL
-	dZP        qKRb
-   dZP          qKKb
-  fZP            SMMb
-  HZM            MMMM
-  FqM            MMMM
-__| ".        |\dS"qML
-|    `.       | `' \\Zq
-_)      \\.___.,|     .'
-\\____   )MMMMMP|   .'
-	`-'       `--'
-[ENTER YOUR NAME]:',
-	}
+func handleconection(client net.Conn, auth *Authentication, limiteure chan struct{}) {
 	defer client.Close()
 	con := bufio.NewReader(client)
-	client.Write([]byte(w.str))
+	s, err := os.ReadFile("file.txt")
+	if err != nil {
+		fmt.Println("Error to Read data")
+	}
+	client.Write([]byte(s))
+	fmt.Fprintf(client, "[ENTER YOUR NAME]: ")
 	str, err := con.ReadString('\n')
 	if err != nil {
-		fmt.Println("hello dumb")
+		fmt.Println("Error to read data from client !")
 	}
 	name := strings.TrimSpace(str)
-	// her i enregister the users and her connection into map !!
+	// Her i enregister the users and There connection into map !!
 	auth.con[name] = &Link{conn: client}
-	// her i send Welcome message to every one login into server !!
-	auth.msg <- Message {
-		Login: "SUDO",
-		content: "You welcome to groube of chat" + name ,
+	times := time.Now().Format("[2006-01-02 15:04:05]")
+	result := fmt.Sprintf("%s [%s]: ", times, name)
+	fmt.Fprintf(client, result)
+	// Her i send Welcome message to every one login into server !!
+	for n, link := range auth.con {
+		if n != name {
+			link.conn.Write([]byte("\n" + name + " has joined our chat..." + "\n"))
+		}
+	}
+	for n, link := range auth.con {
+		if n != name {
+			times := time.Now().Format("[2006-01-02 15:04:05]")
+			result := fmt.Sprintf("%s [%s]: ", times, n)
+			fmt.Fprintf(link.conn, result)
+		}
 	}
 	for {
-		chat , err := con.ReadString('\n')
+		chat, err := con.ReadString('\n')
 		if err != nil {
-			fmt.Println("there is no connection the user left groube")
+			s := "\n" + name + " has left our chat..."
+			for _, link := range auth.con {
+				link.conn.Write([]byte(s))
+			}
+			for n, link := range auth.con {
+				times := time.Now().Format("[2006-01-02 15:04:05]")
+				result := fmt.Sprintf("%s [%s]: ", "\n"+times, n)
+				fmt.Fprintf(link.conn, result)
+			}
+			// I Move the reservation from channel!!
+			<-limiteure
+			// i delete the conection of the client who is left !
+			delete(auth.con, name)
+			// TODO: send message to all client if the client it's left the chatroom!
+			break
 		}
 		cha := strings.TrimSpace(chat)
 		auth.msg <- Message{
-			Login: name,
+			Login:   name,
 			content: cha,
 		}
 	}
